7d0464d3f62f641e9ef1bee0699789c18afd1df8
libra guest <kenjiru.ro@gmail.com>
2012-08-29 16:13:59 -0400

    Enable the transitions.

--- public/js/globalconfig.js
+++ public/js/globalconfig.js
@@ -5,7 +5,7 @@
 
 $(document).bind("mobileinit", function() {
     // disable page transition as they are buggy on Android
-    $.mobile.defaultPageTransition = 'none';
+//    $.mobile.defaultPageTransition = 'none';
     $.mobile.page.prototype.options.addBackBtn = true;
     $.mobile.useFastClick = false;
 });
